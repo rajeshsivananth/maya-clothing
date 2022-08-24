4ee50f7e0e54b58b43a453a97e189475bf0e3dba
Rajesh Kumar <rajeshsivananth@gmail.com>
2022-08-24 20:40:27 +0530

    Added styles and component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
+import './categories.styles.scss';
+import CategoryItem from './component/category-item/category-item.component';
+
 const App = () => {
   const categories = [{
     id: 1,
-    title: 'Hats'
+    title: 'Hats',
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
   }, {
     id: 2,
-    title: 'Jackets'
+    title: 'Jackets',
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
   }, {
     id: 3,
-    title: 'Sneakers'
+    title: 'Sneakers',
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
   }, {
     id: 4,
-    title: 'Mens'
+    title: 'Mens',
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
   }, {
     id: 5,
-    title: 'Womens'
+    title: 'Womens',
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
   }];
   return (
     <div className="categories-container">
       {categories.map((category) => (
-        <div className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+        <CategoryItem category={category} />
       ))
       }
     </div>
